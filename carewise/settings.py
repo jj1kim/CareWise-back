b61ca750e40c5bead66d98d963bd575913d65be6
김지원(Jiwon Kim) <134778013+jj1kim@users.noreply.github.com>
2024-07-16 07:57:12 +0900

    test

--- carewise/settings.py
+++ carewise/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
 
 with open(secret_file) as f:
-    secrets = json.loads(f.read())
+    secrets = json.loads(f)
 
 def get_secret(setting):
     """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
